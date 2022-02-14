f33e5ad7cbd17b0c65669f5285f2d8a4ccfffbe2
xieyang <y.xie@9ebang.com>
2022-02-14 09:55:21 +0800

    测试打包

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-这是一个Vue2的UI库 `nmmc-ui` 指南
-主要运用 vuepress 搭建（新技能Get！）
+# nmmc-ui 指南代码
 
+`nmmc-ui`是一个基于Vue2的UI库,这是指南的代码 
+
+主要运用 vuepress 搭建（新技能Get！）
---
+++ docs/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'nmmc-ui',
+  description: '我们的目标是搞事情~',
+  themeConfig: {
+    nav:[ // 导航栏配置
+      {text: '首页', link: '/' },
+      {text: '技术文档', link: '/guide/' },
+      {text: 'github', link: 'https://github.com/axiea5/nmmc-ui'}      
+    ],
+    sidebar: 'auto', // 侧边栏配置
+  }
+}
